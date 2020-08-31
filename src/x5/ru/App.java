f3af1d936428d31321ccae7bdba5f6342200ff57
Николай <nngitalov@yandex.ru>
2020-08-31 22:11:23 +0300

    Добавил класс App

--- src/x5/ru/App.java
+++ src/x5/ru/App.java
@@ -3,7 +3,7 @@ package x5.ru;
 public class App {
     public static void main (String[] args){
         String s = "Hello";
-        String s2 = "World!!!";
+        String s2 = "World!";
         System.out.println(s+" "+s2);
     }
 }
