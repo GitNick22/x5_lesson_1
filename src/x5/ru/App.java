83af63f4e41e8acd7bb6e9acb2bcb40f2168f296
Николай <nngitalov@yandex.ru>
2020-09-08 02:33:22 +0300

    Обновил текст

--- src/x5/ru/App.java
+++ src/x5/ru/App.java
@@ -2,8 +2,8 @@ package x5.ru;
 
 public class App {
     public static void main (String[] args){
-        String s = "Hello";
-        String s2 = "World!";
+        String s = "Привет ";
+        String s2 = "Мир!";
         System.out.println(s+" "+s2);
     }
 }
